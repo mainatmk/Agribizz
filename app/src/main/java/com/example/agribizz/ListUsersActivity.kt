9fbc546173f3665de502c6fc24777f1541bc774c
mainatmk <terrencem269@gmail.com>
2021-07-12 15:45:59 -0700

    update1

---
+++ app/src/main/java/com/example/agribizz/ListUsersActivity.kt
@@ -0,0 +1,11 @@
+package com.example.agribizz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListUsersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_users)
+    }
+}
