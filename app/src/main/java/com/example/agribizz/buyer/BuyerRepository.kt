8937b64e672b3fb286c23b93281f441d1b964731
mainatmk <terrencem269@gmail.com>
2021-08-09 10:11:32 +0300

    agribizz

--- app/src/main/java/com/example/agribizz/buyer/BuyerRepository.kt
+++ app/src/main/java/com/example/agribizz/buyer/BuyerRepository.kt
@@ -1,20 +1,19 @@
 package com.example.agribizz.buyer
 
 import androidx.lifecycle.LiveData
-import com.example.agribizz.register.RegisterDao
-import com.example.agribizz.register.UserRegister
 
 class BuyerRepository
     (private val buyerdao: BuyerDao) {
 
     val readAllData: LiveData<List<Buyer>> = buyerdao.readAllData()
 
-    suspend fun addUser(buyer: BuyerRepository) {
+    suspend fun addUser(buyer: Buyer) {
         buyerdao.addUser(buyer)
     }
 
     fun fetchdata(): LiveData<Buyer> {
         return buyerdao.getdata()
+
     }
 
 }
--- app/src/main/java/com/example/agribizz/buyer/BuyerViewModel.kt
+++ app/src/main/java/com/example/agribizz/buyer/BuyerViewModel.kt
@@ -6,15 +6,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.agribizz.data.database
-import com.example.agribizz.data.user
-import com.example.agribizz.register.RegisterRepository
-import com.example.agribizz.register.UserRegister
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class BuyerViewModel (application: Application): AndroidViewModel(application){
+class BuyerViewModel (application: Application): AndroidViewModel(application) {
 
-    val readAllData : LiveData<List<Buyer>>
+    val readAllData: LiveData<List<Buyer>>
 
     private val repository: BuyerRepository
     private val readuserobservable = MediatorLiveData<Buyer>()
